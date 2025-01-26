3fd8db044f53c02f79541742ae5059ae083972d6
john4IK-Oleksij <144729031+john4IK-Oleksij@users.noreply.github.com>
2025-01-26 18:35:52 +0100

    Delete lesoon-02 directory

--- lesoon-02/script.js
+++
@@ -1,5 +0,0 @@
-//Задача 1. Знайти суму, добуток та частку двох дійсних чисел. Результат вивести у формі таблиці
-let num1 = parseInt(prompt(`Pirwsza liczba`, `0`));
-let num2 = parseInt(prompt(`druga liczba`, `0`));
-let sum = num1 + num2;
-document.write(`suma= ${sum}`);
